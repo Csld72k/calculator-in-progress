84b360c05c6668f2ce186158ae21f5323db5e4e6
Claudiney Silva <csld72k@gmail.com>
2022-07-30 21:22:10 -0300

    add variable to save the buttons values, add a method to the button clear-all, add a method setError() and set it to the created switch with value default, add a atribute this.operation witch empty array to save the math values, add a method to add values to the array mentioned above

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
   constructor() {
+    this._operation = [];
     this._displayCalcElement = document.querySelector('#display-2');
     this._dataAtual;
     this.initialize();
@@ -19,11 +20,77 @@ class CalcController {
 
   }
 
+  clearAll() {
+    this._operation = [];
+  }
+
+  clearEntry() {
+    this._operation.pop();
+  }
+
+  addOperation(value) {
+    this._operation.push(value);
+    console.log(this._operation);
+  }
+
+  setError() {
+    this.displayCalc = 'Error'
+  }
+
+  execBtn(value) {
+    switch (value) {
+      case 'percentage':
+        break;
+      case 'clear-entry':
+        this.clearEntry();
+        break;
+      case 'clear-all':
+        this.clearAll();
+        break;
+      case '1-bar-x':
+        break;
+      case 'x-squared':
+        break;
+      case 'square-root':
+        break;
+      case 'division':
+        break;
+      case 'multiplication':
+        break;
+      case 'minus':
+        break;
+      case 'plus':
+        break;
+      case 'plus-or-minus':
+        break;
+      case 'comma':
+        break;
+      case 'equal':
+        break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.addOperation(parseInt(value));
+        break;
+      default:
+        this.setError();
+    }
+  }
+
   initButtonsEvents() {
     let buttons = document.querySelectorAll('.keyboard > button');
     buttons.forEach((btn, index) => {
       btn.addEventListener('click', e => {
-        console.log(btn.id.replace('btn-', ''));
+        let textBtn = (btn.id.replace('btn-', ''));
+        console.log(textBtn);
+        this.execBtn(textBtn);
       })
     })
   }
